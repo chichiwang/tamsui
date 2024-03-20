3adc051a724f69b9b7e212784da5d77cb9926697
Chi-chi Wang <chichi@wangchiyi.com>
2024-03-19 22:16:26 -0500

    [jest.config.js] Add separate project configurations for client/server/app
    [server:tests:index] Remove jest-env comment flag, no longer necessary

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,45 @@
+const sharedConfigs = {
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+    '\\.mjs?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^app/(.*)$': '<rootDir>/app/$1',
+  },
+};
+
+const appConfig = {
+  displayName: 'App Tests',
+  testMatch: [
+    '<rootDir>/app/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/app/**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  testEnvironment: 'jsdom',
+  ...sharedConfigs,
+};
+
+const clientConfig = {
+  displayName: 'Client Tests',
+  testMatch: [
+    '<rootDir>/client/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/client/**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  testEnvironment: 'jsdom',
+  ...sharedConfigs,
+};
+
+const serverConfig = {
+  displayName: 'Server Tests',
+  testMatch: [
+    '<rootDir>/server/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/server/**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  testEnvironment: 'node',
+  ...sharedConfigs,
+};
+
 module.exports = {
+  projects: [clientConfig, serverConfig, appConfig],
   collectCoverage: true,
   coverageDirectory: '__coverage__',
   coverageThreshold: {
@@ -9,12 +50,4 @@ module.exports = {
       statements: 100,
     },
   },
-  transform: {
-    '\\.[jt]sx?$': 'babel-jest',
-    '\\.mjs?$': 'babel-jest',
-  },
-  moduleNameMapper: {
-    '^app/(.*)$': '<rootDir>/app/$1',
-  },
-  testEnvironment: 'jsdom',
 };
