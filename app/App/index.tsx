eabf922ecf72e0330ee661fa583332190a577115
Chi-chi Wang <chichi@wangchiyi.com>
2024-04-23 17:52:10 -0500

    [app:App] Add link tags for favicons and `site.webmanifest`
    [app:Home] Display the Tamsui logo, change heading to read "Tamsui"

--- app/App/index.tsx
+++ app/App/index.tsx
@@ -14,6 +14,10 @@ export default function App({ manifest, children }: AppProps): React.ReactElemen
         <head>
           <title>Hello, React SSR!</title>
           <link rel="stylesheet" type="text/css" href={`/${manifest['app.css']}`} />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/images/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/static/site.webmanifest" />
         </head>
         <body>
           { children }
