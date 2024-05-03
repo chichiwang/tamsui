d0ae36fd52587d9108a34910fbeee898bcd483b5
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-03 00:05:27 -0500

    [pages:components:Logo:tests] Update tests for `Logo` component: add snapshots for various permutations of props being passed into the component

--- pages/components/Logo/__tests__/index.test.js
+++ pages/components/Logo/__tests__/index.test.js
@@ -4,8 +4,32 @@ import renderer from 'react-test-renderer';
 import Logo from '../index';
 
 describe('Logo component', () => {
-  test('matches snapshot', () => {
+  test('matches the no-props snapshot', () => {
     const tree = renderer.create(<Logo />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('matches the width prop snapshot', () => {
+    const tree = renderer.create(<Logo width="55px" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('matches the height prop snapshot', () => {
+    const tree = renderer.create(<Logo height="200%" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('matches the width and height props snapshot', () => {
+    const tree = renderer.create(<Logo width="50rem" height="54rem" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('matches the className prop snapshot', () => {
+    const tree = renderer.create(<Logo className="myLogoClass" />).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
