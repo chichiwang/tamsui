c717b81f9e8e588c87d0361a0ed03a01c3231174
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-04 16:16:49 -0500

    [webpack:rules:sass.loaders] Add CSS loader option to provide the css ident name in development mode, hash the ident name in production mode

--- webpack/rules/sass.loaders.js
+++ webpack/rules/sass.loaders.js
@@ -1,6 +1,11 @@
+const env = require('../env');
+
 const loaders = [{
   loader: 'css-loader',
   options: {
+    modules: {
+      localIdentName: env.isProd ? '[hash:base64]' : '[path][name]__[local]',
+    },
     sourceMap: true,
   },
 },
