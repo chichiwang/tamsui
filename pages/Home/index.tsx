69e8ecefa0559d3c7d3bb4ba0e2d1f987ce6fafe
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-06 23:46:19 -0500

    [pages:Home] Remove the `Logo` component and replace it with the `AnimatedLogo` component

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import Layout from 'pages/Layout';
-import Logo from 'pages/components/Logo';
+import AnimatedLogo from 'pages/components/Logo/AnimatedLogo';
 
 import styles from './styles.module.scss';
 
 export default function Home(): React.ReactElement {
   return (
     <Layout>
-      <Logo className={styles.logo} width="200px" />
+      <AnimatedLogo className={styles.logo} width="200px" />
       <h1 className={styles.heading}>Tamsui</h1>
     </Layout>
   );
