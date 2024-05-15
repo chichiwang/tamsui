2b804cdd978b647065bea542a1033cdd9a7b34c0
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-15 07:46:10 -0500

    [pages:components:HeroBanner:SceneMaroon] Create a `SceneMaroon` component to be used in `HeroBanner`

---
+++ pages/components/HeroBanner/SceneMaroon/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+function SceneMaroon() {
+  return (
+    <div className={styles.field}>
+      <div className={styles.scene} />
+    </div>
+  );
+}
+
+export default SceneMaroon;
