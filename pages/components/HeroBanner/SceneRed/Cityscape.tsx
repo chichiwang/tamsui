7fb4073af22052ec5f4024613dfc57aea0f291d6
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-15 07:52:55 -0500

    [pages:components:HeroBanner:SceneRed:TaipeiBuilding] Create a `TaipeiBuilding` component to be used in `Cityscape`

--- pages/components/HeroBanner/SceneRed/Cityscape.tsx
+++ pages/components/HeroBanner/SceneRed/Cityscape.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import TaipeiBuilding from './TaipeiBuilding';
+
 import styles from './styles.module.scss';
 
 function Cityscape() {
   return (
     <>
-      <div className={styles.fiveBuilding}>
-        <div className={styles.base} />
-        <div className={classNames(styles.level, styles.one)} />
-        <div className={classNames(styles.level, styles.two)} />
-        <div className={classNames(styles.level, styles.three)} />
-        <div className={classNames(styles.level, styles.four)} />
-        <div className={classNames(styles.level, styles.five)} />
-        <div className={classNames(styles.level, styles.six)} />
-        <div className={classNames(styles.level, styles.seven)} />
-        <div className={classNames(styles.level, styles.eight)} />
-        <div className={styles.topBase} />
-        <div className={styles.antenna} />
-        <div className={styles.sign} />
-      </div>
-
+      <TaipeiBuilding />
       <div className={styles.cappedBldg} />
       <div className={classNames(styles.cappedBldg, styles.bldg01)} />
       <div className={classNames(styles.cappedBldg, styles.bldg02)} />
