dc1984fb88a203977f4fe10177ade0012003970b
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-17 02:17:06 -0500

    [pages:components:HeroBanner:SceneRed:MountainRange] Create a component `MountainRange` to be used in `SceneRed`

--- pages/components/HeroBanner/SceneRed/index.tsx
+++ pages/components/HeroBanner/SceneRed/index.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 import Cityscape from './Cityscape';
+import MountainRange from './MountainRange';
 
 function SceneRed() {
   return (
     <div className={styles.field}>
       <div className={styles.scene}>
         <Cityscape />
+        <MountainRange />
       </div>
     </div>
   );
