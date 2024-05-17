a6b3bb0cd7184947981a19e6149873ff28c97551
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-17 02:17:06 -0500

    [pages:components:HeroBanner:SceneRed:MountainRange] Create a component `MountainRange` to be used in `SceneRed`

---
+++ pages/components/HeroBanner/SceneRed/MountainRange.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import styles from './styles.mountainrange.module.scss';
+
+function MountainRange() {
+  return (
+    <div className={styles.mountainRange}>
+      <div className={styles.m01} />
+      <div className={styles.m02} />
+      <div className={styles.m03} />
+      <div className={styles.m04} />
+      <div className={styles.m05} />
+      <div className={styles.m06} />
+      <div className={styles.m07} />
+      <div className={styles.m08} />
+      <div className={styles.m09} />
+      <div className={styles.m10} />
+      <div className={styles.m11} />
+      <div className={styles.m12} />
+      <div className={styles.m13} />
+    </div>
+  );
+}
+
+export default MountainRange;
