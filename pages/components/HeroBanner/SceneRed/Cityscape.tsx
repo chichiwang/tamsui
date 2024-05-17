da1075782bf72f486873ae24b41827f87dff31fc
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-17 02:53:59 -0500

    [pages:components:HeroBanner:SceneRed] Clean up `styles.cityscape.module.scss` to encapsulate entire cityscape in a single container

--- pages/components/HeroBanner/SceneRed/Cityscape.tsx
+++ pages/components/HeroBanner/SceneRed/Cityscape.tsx
@@ -3,21 +3,21 @@ import classNames from 'classnames';
 
 import TaipeiBuilding from './TaipeiBuilding';
 
-import styles from './styles.module.scss';
+import styles from './styles.cityscape.module.scss';
 
 function Cityscape() {
   return (
-    <>
+    <div className={styles.cityscape}>
+      <div className={classNames(styles.cappedBldg, styles.bldg01)} />
+      <div className={classNames(styles.flatBldg, styles.bldg01)} />
+      <div className={classNames(styles.domedBldg, styles.bldg01)} />
+      <div className={classNames(styles.flatBldg, styles.bldg02)} />
       <TaipeiBuilding />
       <div className={styles.cappedBldg} />
-      <div className={classNames(styles.cappedBldg, styles.bldg01)} />
-      <div className={classNames(styles.cappedBldg, styles.bldg02)} />
       <div className={styles.flatBldg} />
-      <div className={classNames(styles.flatBldg, styles.bldg01)} />
-      <div className={classNames(styles.flatBldg, styles.bldg02)} />
+      <div className={classNames(styles.cappedBldg, styles.bldg02)} />
       <div className={styles.domedBldg} />
-      <div className={classNames(styles.domedBldg, styles.bldg01)} />
-    </>
+    </div>
   );
 }
 
