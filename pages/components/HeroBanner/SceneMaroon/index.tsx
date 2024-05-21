76855d8bc4bb710e43f6800837410ea1b35d5b25
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-20 22:04:52 -0500

    [pages:components:HeroBanner:SceneMaroon:CloudscapeBackground] Create component `CloudscapeBackground` to be used in component `SceneMaroon`
    [pages:components:HeroBanner:SceneOrange:CloudscapeForeground]
    Reposition clouds, update animation timing

--- pages/components/HeroBanner/SceneMaroon/index.tsx
+++ pages/components/HeroBanner/SceneMaroon/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import CloudscapeBackground from './CloudscapeBackground';
+
 import styles from './styles.module.scss';
 
 function SceneMaroon() {
   return (
     <div className={styles.field}>
-      <div className={styles.scene} />
+      <div className={styles.scene}>
+        <CloudscapeBackground />
+      </div>
     </div>
   );
 }
