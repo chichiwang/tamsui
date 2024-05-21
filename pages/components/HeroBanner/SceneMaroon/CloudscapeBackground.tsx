9391b371cd15d67cf8b93e1723fb1b6137247191
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-20 22:04:52 -0500

    [pages:components:HeroBanner:SceneMaroon:CloudscapeBackground] Create component `CloudscapeBackground` to be used in component `SceneMaroon`
    [pages:components:HeroBanner:SceneOrange:CloudscapeForeground]
    Reposition clouds, update animation timing

---
+++ pages/components/HeroBanner/SceneMaroon/CloudscapeBackground.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import styles from './styles.cloudscapebackground.module.scss';
+
+function CloudscapeBackground() {
+  return (
+    <div className={classNames(styles.cloudscape, styles.float)}>
+
+      <div className={classNames(styles.container, styles.offscreen)}>
+        <div className={styles.cloud01}>
+          <div className={styles.c01} />
+          <div className={styles.c02} />
+          <div className={styles.c03} />
+        </div>
+
+        <div className={styles.cloud02}>
+          <div className={styles.c01} />
+          <div className={styles.c02} />
+          <div className={styles.c03} />
+        </div>
+
+        <div className={classNames(styles.cloud01, styles.pos01)}>
+          <div className={styles.c01} />
+          <div className={styles.c02} />
+          <div className={styles.c03} />
+        </div>
+
+        <div className={classNames(styles.cloud02, styles.pos01)}>
+          <div className={styles.c01} />
+          <div className={styles.c02} />
+          <div className={styles.c03} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.cloud01}>
+          <div className={styles.c01} />
+          <div className={styles.c02} />
+          <div className={styles.c03} />
+        </div>
+
+        <div className={styles.cloud02}>
+          <div className={styles.c01} />
+          <div className={styles.c02} />
+          <div className={styles.c03} />
+        </div>
+
+        <div className={classNames(styles.cloud01, styles.pos01)}>
+          <div className={styles.c01} />
+          <div className={styles.c02} />
+          <div className={styles.c03} />
+        </div>
+
+        <div className={classNames(styles.cloud02, styles.pos01)}>
+          <div className={styles.c01} />
+          <div className={styles.c02} />
+          <div className={styles.c03} />
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default CloudscapeBackground;
