9c565509c7adef4139b37e51a02ae902bc372aed
Chi-chi Wang <chichi@wangchiyi.com>
2024-05-30 20:01:06 -0500

    [pages:Documentation] Wrap all Documentation page contents in a `ContentBlock`

--- pages/Documentation/index.tsx
+++ pages/Documentation/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import ContentBlock from 'pages/components/ContentBlock';
+
 function Documentation() {
   return (
-    <>
+    <ContentBlock>
       <h1>Developer Documentation</h1>
       <p>Work in Progress</p>
-    </>
+    </ContentBlock>
   );
 }
 
