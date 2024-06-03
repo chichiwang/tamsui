7176d8b0e6a7565749016b35033eb068338d6808
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-03 16:49:31 -0500

    [pages:components:ChecklistItem] Create component `ChecklistItem`
    [pages:Documentation] Add a `ChecklistItem` component to the
    Documentation page (for development purposes)

--- pages/Documentation/index.tsx
+++ pages/Documentation/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import ContentBlock from 'pages/components/ContentBlock';
+import ChecklistItem from 'pages/components/ChecklistItem';
 
 function Documentation() {
   return (
     <ContentBlock>
       <h1>Developer Documentation</h1>
-      <p>Work in Progress</p>
+      <ChecklistItem>Work in Progress</ChecklistItem>
     </ContentBlock>
   );
 }
