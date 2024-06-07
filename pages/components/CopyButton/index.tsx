6a3e037e642e9e483400450186f4bd60f203a7e3
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-07 01:14:48 -0500

    [pages:Documentation] Render `CopyButton` instead of `CopyIcon` (for development purposes)
    [pages:components:CopyButton] Create `CopyButton` component

---
+++ pages/components/CopyButton/index.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+
+import CopyIcon from 'pages/components/CopyIcon';
+
+import styles from './styles.module.scss';
+
+type CopyButtonProps = {
+  textToCopy: string;
+};
+
+enum CopyStates {
+  ready,
+  copying,
+  copied,
+  errored
+}
+
+const ButtonContent = {
+  [CopyStates.ready]: (<CopyIcon className={styles.copyIcon} />),
+  [CopyStates.copying]: (<div className={styles.copyCharacter}>⋯</div>),
+  [CopyStates.copied]: (<div className={styles.copyCharacter}>✓</div>),
+  [CopyStates.errored]: (<div className={styles.copyCharacter}>!</div>),
+};
+
+const messageDisplayTime = 2000;
+
+function CopyButton({
+  textToCopy,
+}: CopyButtonProps) {
+  const [copyState, setCopyState] = useState(CopyStates.ready);
+
+  const isButtonDisabled = copyState !== CopyStates.ready;
+
+  function resetButton() {
+    setCopyState(CopyStates.ready);
+  }
+
+  function writeToClipboard() {
+    setCopyState(CopyStates.copying);
+
+    const writePromise = navigator.clipboard.writeText(textToCopy);
+
+    writePromise
+      .then(function writeComplete() {
+        setCopyState(CopyStates.copied);
+        setTimeout(resetButton, messageDisplayTime);
+      })
+      .catch(function writeFailed(err) {
+        console.error(err);
+        setCopyState(CopyStates.errored);
+        setTimeout(resetButton, messageDisplayTime);
+      });
+  }
+
+  const containerClassname = classNames({
+    [styles.copyContainer]: true,
+    [styles.disabled]: copyState === CopyStates.copying,
+    [styles.copied]: copyState === CopyStates.copied,
+    [styles.error]: copyState === CopyStates.errored,
+  });
+
+  const buttonProps = {
+    className: styles.copyButton,
+    'aria-label': 'Copy text',
+    ...(isButtonDisabled ? {} : {
+      onClick: writeToClipboard,
+    }),
+  };
+
+  return (
+    <div className={styles.copyPosition}>
+      <div className={containerClassname}>
+        <div className={styles.message}>
+          <div className={styles.successMessage}>
+            <span>Copied!</span>
+          </div>
+          <div className={styles.errorMessage}>
+            <span>Failed!</span>
+          </div>
+        </div>
+        <button type="button" {...buttonProps}>
+          { ButtonContent[copyState] }
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CopyButton;
