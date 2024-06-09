6ae2cb66232ac700d8b3cd0142c001dbaa5bac9e
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-09 18:32:51 -0500

    [pages:Documentation] Add section: Adding an Application Directory

--- pages/Documentation/index.tsx
+++ pages/Documentation/index.tsx
@@ -371,10 +371,17 @@ function Documentation() {
           <li>
             <a href="#Project-Directories">Project Directories</a>
           </li>
+          <li>
+            <a href="#Adding-an-Application-Directory">Adding an Application Directory</a>
+          </li>
         </ul>
       </HeadingBlock>
 
-      <HeadingBlock level="3" id="Running-the-Application" heading="Running the Application">
+      <HeadingBlock
+        level="3"
+        id="Running-the-Application"
+        heading="Running the Application"
+      >
         <p>
           {'Run '}
           <span className={styles.highlight}>npm run watch</span>
@@ -391,7 +398,11 @@ function Documentation() {
         </p>
       </HeadingBlock>
 
-      <HeadingBlock level="3" id="Project-Directories" heading="Project Directories">
+      <HeadingBlock
+        level="3"
+        id="Project-Directories"
+        heading="Project Directories"
+      >
         <p>
           {'The '}
           <span className={styles.highlight}>client/</span>
@@ -444,6 +455,105 @@ function Documentation() {
           </ExternalLink>
           {' is implemented as the logger.'}
         </p>
+      </HeadingBlock>
+
+      <HeadingBlock
+        level="3"
+        id="Adding-an-Application-Directory"
+        heading="Adding an Application Directory"
+      >
+        <p>
+          To add additional application directories, or to remove existing ones:
+        </p>
+
+        <ul className={styles.list}>
+          <li>Path aliases need to be updated</li>
+          <li>Jest projects need to be updated</li>
+        </ul>
+
+        <p>
+          {'The '}
+          <span className={styles.highlight}>app/</span>
+          {' and '}
+          <span className={styles.highlight}>pages/</span>
+          {` directories are set up with path aliases so that a module can be imported with
+           absolute pathing rather than relative pathing:`}
+        </p>
+
+        <div className={styles.codeBlock}>
+          <CopyButton
+            className={styles.copyButton}
+            textToCopy={'import Module from app/Module;\nimport PageModule from page/PageModule;'}
+          />
+          <p>
+            <span className={styles.red}>import</span>
+            {' Module '}
+            <span className={styles.red}>from</span>
+            {' '}
+            <span className={styles.blue}>&apos;app/module&apos;</span>
+            ;
+          </p>
+          <p>
+            <span className={styles.red}>import</span>
+            {' PageModule '}
+            <span className={styles.red}>from</span>
+            {' '}
+            <span className={styles.blue}>&apos;page/PageModule&apos;</span>
+            ;
+          </p>
+        </div>
+
+        <p>
+          If any directory is removed, or if a new directory needs an alias:
+        </p>
+
+        <ul className={styles.list}>
+          <li>
+            {'An '}
+            <span className={styles.highlight}>alias</span>
+            {' should be updated in the '}
+            <ExternalLink href="https://github.com/chichiwang/tamsui/blob/main/webpack/resolve.js">
+              Webpack configs
+            </ExternalLink>
+            .
+          </li>
+          <li>
+            {'The '}
+            <ExternalLink href="https://github.com/johvin/eslint-import-resolver-alias?tab=readme-ov-file#usage">
+              alias mapping
+            </ExternalLink>
+            {' should be added to the '}
+            <ExternalLink href="https://github.com/chichiwang/tamsui/blob/main/.eslintrc.js">
+              eslint configuration
+            </ExternalLink>
+            {' to avoid linting errors when using the alias.'}
+          </li>
+          <li>
+            {'The alias should be updated in the '}
+            <ExternalLink href="https://www.typescriptlang.org/tsconfig#paths">
+              paths configuration
+            </ExternalLink>
+            {' for TypeScript to avoid type errors.'}
+          </li>
+          <li>
+            {'The alias should be updated in the '}
+            <ExternalLink href="https://jestjs.io/docs/configuration#modulenamemapper-objectstring-string--arraystring">
+              moduleNameMapper configuration
+            </ExternalLink>
+            {' in the '}
+            <ExternalLink href="https://github.com/chichiwang/tamsui/blob/main/jest.config.js">
+              Jest configuration
+            </ExternalLink>
+            {' to ensure module mocking will work in tests, and so there are no errors in using the alias.'}
+            <ul className={styles.list}>
+              <li>
+                {'The directory should be updated in the '}
+                <span className={styles.highlight}>appConfig.testMatch</span>
+                {' array to ensure that the test runner knows which directories to cover.'}
+              </li>
+            </ul>
+          </li>
+        </ul>
       </HeadingBlock>
     </ContentBlock>
   );
