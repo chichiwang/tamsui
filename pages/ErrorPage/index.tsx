0f555af0027e417dc7f0850dd67f231824ad884f
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-11 19:38:33 -0500

    [pages:ErrorPage] Add animated logo and error message to page

--- pages/ErrorPage/index.tsx
+++ pages/ErrorPage/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import ContentBlock from 'app/components/ContentBlock';
+import AnimatedLogo from 'app/components/Logo/AnimatedLogo';
+
+import styles from './styles.module.scss';
 
 export default function ErrorPage() {
   return (
-    <>
-      <h1>An Error Occurred!</h1>
-      <Link to="/">Home</Link>
-    </>
+    <ContentBlock>
+      <div className={styles.logoBlock}>
+        <AnimatedLogo height="250px" />
+      </div>
+      <h1 className={styles.errorHeading}>An Error Occurred!</h1>
+      <p className={styles.errorMessage}>
+        Unfortunately something has gone wrong, apologies for the inconvenience.
+      </p>
+    </ContentBlock>
   );
 }
