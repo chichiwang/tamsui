2750772d78b54b2963e06fc40dfedf05a3e345c3
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-11 19:51:13 -0500

    [pages:NotFound] Add animated logo and not found message to page

--- pages/NotFound/index.tsx
+++ pages/NotFound/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import ContentBlock from 'app/components/ContentBlock';
+import AnimatedLogo from 'app/components/Logo/AnimatedLogo';
+
+import styles from './styles.module.scss';
+
 export default function NotFound(): React.ReactElement {
   return (
-    <>
-      <h1>Page Not Found!</h1>
-      <Link to="/">Return to Home</Link>
-    </>
+    <ContentBlock>
+      <div className={styles.logoBlock}>
+        <AnimatedLogo height="250px" />
+      </div>
+      <h1 className={styles.notFoundHeading}>Page Not Found!</h1>
+      <p className={styles.notFoundMessage}>
+        This page does not exit!
+      </p>
+      <p className={styles.notFoundMessage}>
+        <Link to="/">Return to Home</Link>
+      </p>
+    </ContentBlock>
   );
 }
