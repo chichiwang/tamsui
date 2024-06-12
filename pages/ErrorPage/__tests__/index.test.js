972dd0b9a470318d3bcab64f36567eec8153356b
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-11 19:38:33 -0500

    [pages:ErrorPage] Add animated logo and error message to page

--- pages/ErrorPage/__tests__/index.test.js
+++ pages/ErrorPage/__tests__/index.test.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import ErrorPage from '../index';
 
-function RouterWrappedErrorPage() {
-  return (
-    <MemoryRouter>
-      <ErrorPage />
-    </MemoryRouter>
-  );
-}
-
 describe('ErrorPage Component', () => {
   test('matches snapshot', () => {
-    const tree = renderer.create(<RouterWrappedErrorPage />).toJSON();
+    const tree = renderer.create(<ErrorPage />).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
   test('contains the error text', () => {
-    render(<RouterWrappedErrorPage />);
+    render(<ErrorPage />);
 
     expect(screen.getByRole('heading')).toHaveTextContent('An Error Occurred!');
   });
