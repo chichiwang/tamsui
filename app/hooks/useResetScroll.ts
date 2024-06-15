cea49026797c14217c93c9808ec9fe41245a000c
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-15 15:00:29 -0500

    [app:hooks:useResetScroll] If the location state contains a hash that matches an element on the new page, scroll that element into view

--- app/hooks/useResetScroll.ts
+++ app/hooks/useResetScroll.ts
@@ -9,15 +9,24 @@ function useResetScroll() {
 
   useLayoutEffect(function resetScrollPosition() {
     if (location.state?.[ResetScrollKey]) {
-      document.documentElement.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'instant',
-      });
+      const IdFromHash = location.hash.slice(1);
+      const $hashEl = document.getElementById(IdFromHash);
 
-      navigate(location.pathname, { replace: true });
+      if ($hashEl) {
+        $hashEl.scrollIntoView(true);
+      } else {
+        document.documentElement.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'instant',
+        });
+      }
+
+      navigate(`${location.pathname}${location.hash}`, {
+        replace: true,
+      });
     }
-  }, [location.pathname]);
+  }, [location.pathname, location.hash]);
 }
 
 export default useResetScroll;
