7160624d19f3da46ef5bcd69c9c9cb547c27cf3a
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-15 17:11:18 -0500

    [app:components:InternalLink] Refactor test suite to be able to ensure state property behavior

--- app/components/InternalLink/__tests__/index.test.js
+++ app/components/InternalLink/__tests__/index.test.js
@@ -1,27 +1,83 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
+import { ResetScrollKey } from 'app/hooks/useResetScroll';
 import InternalLink from '../index';
 
-function RouterWrappedLink(props) {
+function RenderProp({ k, v }) {
   return (
-    <MemoryRouter>
-      <InternalLink {...props} />
-    </MemoryRouter>
+    <p data-testid={`${k}`}>{v}</p>
   );
 }
 
+function MockedLink(props) {
+  return (
+    <div>
+      { Object.entries(props).map(function renderEachProp([key, value]) {
+        const val = JSON.stringify(value);
+        return <RenderProp key={key} k={key} v={val} />;
+      }) }
+    </div>
+  );
+}
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  Link: MockedLink,
+}));
+
+const expectedStateContent = {
+  [ResetScrollKey]: true,
+};
+
 describe('InternalLink component', () => {
   test('matches the snapshot', () => {
-    const tree = renderer.create(<RouterWrappedLink to="/test" />).toJSON();
+    const tree = renderer.create(<InternalLink to="/test" />).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
-  test('matches the snapshot with existing state', () => {
-    const tree = renderer.create(<RouterWrappedLink to="/test-state" state={{ unrelated: 'stateval' }} />).toJSON();
+  test('passes all properties through to React Router Link', () => {
+    const props = {
+      to: '/test-props',
+      children: 'nested-elements',
+      foo: 'bar',
+      hello: 'compositional component!',
+    };
 
-    expect(tree).toMatchSnapshot();
+    render(<InternalLink {...props} />);
+
+    for (const [key, value] of Object.entries(props)) {
+      const $el = screen.queryByTestId(key);
+
+      expect($el).toBeInTheDocument();
+      expect($el).toHaveTextContent(JSON.stringify(value));
+    }
+  });
+
+  test('adds a state object to trigger scroll reset', () => {
+    const targetPath = '/test-state-applied';
+
+    render(<InternalLink to={targetPath} />);
+
+    expect(screen.queryByTestId('to')).toHaveTextContent(targetPath);
+    expect(screen.queryByTestId('state')).toHaveTextContent(JSON.stringify(expectedStateContent));
+  });
+
+  test('extends provided state object with trigger for scroll reset', () => {
+    const targetPath = '/test-state-extended';
+    const initialStateObject = {
+      some: 'state',
+    };
+
+    render(<InternalLink to={targetPath} state={initialStateObject} />);
+
+    expect(screen.queryByTestId('to')).toHaveTextContent(targetPath);
+    expect(screen.queryByTestId('state')).toHaveTextContent(JSON.stringify({
+      ...initialStateObject,
+      ...expectedStateContent,
+    }));
   });
 });
