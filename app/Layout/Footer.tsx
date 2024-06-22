ce32de00d665710d283f0ef79c122d557c00a38c
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-22 14:13:44 -0500

    [app:Layout:Footer] Animate divider
    [app:Layout:Nav] Consolidate styles with `app/Layout/styles.module.scss`
    and remove `app/Layout/styles.nav.module.scss`

--- app/Layout/Footer.tsx
+++ app/Layout/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import ExternalLink from 'app/components/ExternalLink';
 
@@ -7,7 +8,11 @@ import styles from './styles.module.scss';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.divider} />
+      <div className={styles.divider}>
+        <div className={styles.accentBarContainer}>
+          <div className={classNames(styles.accentBar, styles.slowSlide)} />
+        </div>
+      </div>
       <p>
         {'Created by '}
         <ExternalLink href="https://github.com/chichiwang">
