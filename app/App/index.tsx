76296d7fb51421d3fc366e8a723b0067a5f0ebb9
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-22 15:12:31 -0500

    [app:App] Add meta tags into the `head` tag: author, keywords, description, robots, viewport

--- app/App/index.tsx
+++ app/App/index.tsx
@@ -14,12 +14,19 @@ export default function App({ manifest, children }: AppProps): React.ReactElemen
         <head>
           <meta charSet="utf-8" />
           <title>Tamsui</title>
+          <meta name="author" content="Chi-chi Wang" />
+          <meta
+            name="keywords"
+            content="Tamsui, js, JavaScript, isomorphic, Node.js, React, TypeScript, Express, React-Router, Boilerplate"
+          />
+          <meta name="description" content="A TypeScript React server-side rendering boilerplate" />
+          <meta name="robots" content="all" />
+          <meta name="viewport" content="width=device-width" />
           <link rel="stylesheet" type="text/css" href={`/${manifest['app.css']}`} />
           <link rel="apple-touch-icon" sizes="180x180" href="/static/images/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/static/site.webmanifest" />
-          <meta name="viewport" content="width=device-width" />
         </head>
         <body>
           { children }
