cf51744cca111e0ec14a0f8393e1e65929972d43
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-24 16:14:43 -0500

    [app:hooks:useRouteHead] Create hook `useRouteHead` to return the `head` property of a matched route `handle`
    [app:types.ts] Add types `RouteHead`, `PageHandle`, `PageMatch`

--- app/types.ts
+++ app/types.ts
@@ -1,10 +1,33 @@
+import React from 'react';
+import type { UIMatch } from 'react-router-dom';
+
+type HeadingLevel = '1' | '2' | '3' | '4' | '5' | '6';
+
 type Manifest = {
   [key: string]: string;
 };
 
-type HeadingLevel = '1' | '2' | '3' | '4' | '5' | '6';
+/**
+ * Route Match and Handle types
+ */
+
+type RouteHead = {
+  title?: string;
+  tags?: React.ReactNode;
+};
+
+type PageHandle = {
+  head?: RouteHead;
+};
+
+interface PageMatch extends UIMatch {
+  handle: PageHandle,
+}
 
 export {
   HeadingLevel,
   Manifest,
+  PageMatch,
+  PageHandle,
+  RouteHead,
 };
