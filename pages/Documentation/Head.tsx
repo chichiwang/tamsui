4528f7e93cfce24f6098426a947ce04372d52ad1
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-25 16:30:09 -0500

    [pages:Home:Head] Add canonical link
    [pages:Documentation:Head] Add canonical link

--- pages/Documentation/Head.tsx
+++ pages/Documentation/Head.tsx
@@ -11,6 +11,7 @@ function Head() {
       <meta property="og:title" content="Developer Documentation for Tamsui: React/Express universal JavaScript boilerplate" />
       <meta property="og:description" content="Set up a universal JavaScript application on the Tamsui boilerplate." />
       <meta property="og:url" content={url} />
+      <link href={url} rel="canonical" />
     </>
   );
 }
