4317e136c4eb2baa753bcfbf7d3dc60c71923f85
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-25 14:56:43 -0500

    [project-configs.js] Add environment dependent config `PROJECT_URL`
    [app:globals.d.ts] Declare global `PROJECT_URL`

--- project-configs.js
+++ project-configs.js
@@ -2,9 +2,11 @@ module.exports = {
   development: {
     PORT: 8080,
     SERVE_STATIC: true,
+    PROJECT_URL: 'http://127.0.0.1',
   },
   production: {
     PORT: 3000,
     SERVE_STATIC: false,
+    PROJECT_URL: 'https://tamsui.dev',
   },
 };
