6f5b9146c46139cefe167a8e252681f3f1ff9816
Chi-chi Wang <chichi@wangchiyi.com>
2024-06-25 15:38:15 -0500

    [project-configs.js] Update development-environment config `PROJECT_URL`

--- project-configs.js
+++ project-configs.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     PORT: 8080,
     SERVE_STATIC: true,
-    PROJECT_URL: 'http://127.0.0.1',
+    PROJECT_URL: JSON.stringify('http://127.0.0.1:8080'),
   },
   production: {
     PORT: 3000,
     SERVE_STATIC: false,
-    PROJECT_URL: 'https://tamsui.dev',
+    PROJECT_URL: JSON.stringify('https://tamsui.dev'),
   },
 };
